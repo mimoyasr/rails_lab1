9f04b79dd45126df794745d2fb99f7b494e058a1
mimoyasr <amryasserhamza@gmail.com>
2018-05-01 15:18:21 +0200

    add user full name to user view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :name, presence: true
-    validates :name, uniqueness: scope: [:last_name]
+    validates :name, uniqueness: {scope: [:last_name]}
 
     enum gender: {male: 0, female: 1, any: 2}
 
