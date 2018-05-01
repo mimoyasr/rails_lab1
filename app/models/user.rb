55b02a1d803a762758cbe35902e9e574e6b2285c
mimoyasr <amryasserhamza@gmail.com>
2018-05-01 14:49:59 +0200

    upate user gender add enum and fix its views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-    validates :name, presence :true
+    validates :name, presence: true
+    validates :name, uniqueness: true
+
+    enum gender: {male: 0, female: 1, any: 2}
 end
